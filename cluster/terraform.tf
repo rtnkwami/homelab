5f77c6df5e5682ee5f1607bac4aabbd862d93e2b
rtnkwami <nukukwami@outlook.com>
2026-09-02 16:12:06 +0000

    feat(cluster): add initial vms

--- cluster/terraform.tf
+++ cluster/terraform.tf
@@ -9,5 +9,20 @@ terraform {
       source = "hetznercloud/hcloud"
       version = "~>1.0"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~>4.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "~>3.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.0"
+    }
   }
 }
