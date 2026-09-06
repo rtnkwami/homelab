91cbb3325ed4dd849d9d39db4133d0fc345fbe35
rtnkwami <nukukwami@outlook.com>
2026-09-06 07:39:10 +0000

    feat(cluster): add missing location field to spread controlplane across regions

--- cluster/servers.tf
+++ cluster/servers.tf
@@ -35,6 +35,7 @@ resource "hcloud_server" "controlplane" {
 
   name = "controlplane-${each.value}"
   server_type = "cpx22"
+  location = each.value
   image = data.hcloud_image.talos_x86.id
   ssh_keys = [hcloud_ssh_key.this.id]
   placement_group_id = hcloud_placement_group.controlplane.id
